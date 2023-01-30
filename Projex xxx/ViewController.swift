766171494d14b733aefe17a412f202c739d68b01
bombag456 <bombag456@gmail.com>
2023-01-31 02:52:32 +0600

    Initial Commit

---
+++ Projex xxx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projex xxx
+//
+//  Created by Mac on 31/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
